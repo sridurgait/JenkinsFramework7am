c158caa8b61846ef3271922cf18f08b52147cc60
sridurgait <142955249+sridurgait@users.noreply.github.com>
2023-08-29 09:50:59 +0530

    Update DemoTest.java

--- Maven_Demo/src/test/java/Pratice/DemoTest.java
+++ Maven_Demo/src/test/java/Pratice/DemoTest.java
@@ -12,30 +12,7 @@ public class DemoTest {
 	@Test(groups="smokeTest")
  public void DemoTest()
  {
-		/*WebDriver driver;
-		String Browser=System.getProperty("browser");
-		String URL=System.getProperty("url");
-		if(Browser.equalsIgnoreCase("chrome"))
-			{
-				WebDriverManager.chromedriver().setup();
-				 driver=new ChromeDriver();
-			}
-			else if(Browser.equalsIgnoreCase("firefox"))
-			{
-				WebDriverManager.firefoxdriver().setup();
-				driver=new FirefoxDriver();
-				
-			}
-			else if(Browser.equalsIgnoreCase("msedge"))
-			{
-				WebDriverManager.edgedriver().setup();
-				 driver=new EdgeDriver();
-			}
-			else
-			{
-				driver=new ChromeDriver();
-			}
-	 driver.get(URL);*/
+		
 		System.out.println("hi");
 	 System.out.println("Git Pull Back");
 	 
